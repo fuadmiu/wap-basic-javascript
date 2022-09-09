c23d6caf52e07c203eab5db707c9a8085cddb2be
Fuad <fuadmail@gmail.com>
2022-09-09 10:47:38 -0500

    Unobstrusive JS

--- basic-javascript.js
+++ basic-javascript.js
@@ -86,3 +86,48 @@ function productOfNums() {
     const a = [1,3,5,3,3]; 
     return a.reduce((a, b) => a * b);
 }
+
+
+
+// TEST CODES
+/* runs test to see if expected argument is === to value returned by function2test argument */
+function myFunctionTest(expected, found) {
+    // condition added to compare two array
+    if (expected === found || (Array.isArray(expected) && isArrayEqual(expected, found))) {
+        return "TEST SUCCEEDED";
+    } else {
+        return "TEST FAILED.  Expected " + expected + " found " + found;
+    }
+}
+
+// Additional method to compare equal array.
+function isArrayEqual(array1, array2) {
+    return array1.length === array2.length && array1.every(function(value, index) { return value === array2[index]});
+}
+
+
+
+// Test Cases
+console.log("Expected output of max(20,10) is 20  " + myFunctionTest(20, max(20, 10)));
+
+console.log("Expected output of maxOfThree(5,4,44) is 44  " + myFunctionTest(44, maxOfThree(5, 4, 44)));
+console.log("Expected output of maxOfThree(55,4,44) is 55  " + myFunctionTest(55, maxOfThree(55, 4, 44)));
+
+console.log("Expected output of isVowel('w') is false  " + myFunctionTest(false, isVowel('w')));
+console.log("Expected output of isVowel('e') is true  " + myFunctionTest(true, isVowel('e')));
+
+console.log("Expected output of sum([1,2,3,4]) is 10  " + myFunctionTest(10, sum([1,2,3,4])));
+
+console.log("Expected output of multiply([1,2,3,4]) is 24  " + myFunctionTest(24, multiply([1,2,3,4])));
+
+console.log("Expected output of reverse('jag testar') is ratset gaj  " + myFunctionTest('ratset gaj', reverse('jag testar')));
+
+console.log("Expected output of findLongestWord(['Hello','Javascript', 'World']) is 10  " + myFunctionTest(10, findLongestWord(['Hello','Javascript', 'World'])));
+
+console.log("Expected output of filterLongWords(['web', 'Hello','Javascript', 'World', 'JS', 3]) is ['Hello','Javascript', 'World']  " + myFunctionTest(['Hello','Javascript', 'World'], filterLongWords(['web', 'Hello','Javascript', 'World', 'JS'], 3)));
+
+
+console.log('========== JS Fiddle Example output, Input array [1,3,5,3,3] ===== ');
+console.log("Expected output of multiply each element by 10 is [ 10, 30, 50, 30, 30 ]  " + myFunctionTest([ 10, 30, 50, 30, 30 ], multiplyBy10()));
+console.log("Expected output of return array with all elements equal to 3, is [ 3, 3, 3 ]  " + myFunctionTest([ 3, 3, 3 ], filterVal3()));
+console.log("Expected output of product all elements is 135  " + myFunctionTest(135, productOfNums()));
